b963f0d011f98fb00bf143120ade50584b675a3c
Bgarrix <66459466+bry4ng4rrix@users.noreply.github.com>
2025-07-14 16:31:33 +0300

    Update page.jsx

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -4,7 +4,7 @@ const page = () => {
 
   
   return (
-  <div className="grid  sm:grid-cols-2 gap-4 p-2 m-5 "> 
+  <div className="grid  sm:grid-cols-2 gap-4 p-2 my5 "> 
     <Profile />
 
       <Allblog/>
